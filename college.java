5ba2a6f9ff74c46c85ce8f578ff2f3a6630d4a0b
Prince Kumar Mishra <kmishraprince@gmail.com>
2024-08-20 13:21:39 +0530

    Create college.java

---
+++ college.java
@@ -0,0 +1,17 @@
+class Student{
+    void Fee() {
+        System.out.println("Student Fee= 20000");
+    }
+}
+class Student_Name extends Student{ 
+    void Name() {
+        System.out.println("Student Name=Jayanti");
+    }
+}
+class College {
+    public static void main(String args[]) {
+        Student_Name p = new Student_Name();
+        p.Fee(); 
+        p.Name(); 
+    }
+}
